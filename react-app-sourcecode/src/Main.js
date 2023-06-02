cfab7ac26481d3d60ca2533c37e5a86a1015be7b
codershiyar <codershiyar@gmail.com>
2023-06-02 09:55:11 +0100

    container class verwijderd van main component

--- react-app-sourcecode/src/Main.js
+++ react-app-sourcecode/src/Main.js
@@ -1,8 +1,8 @@
 export default function Main() {
-    return <main className="container">
+    return <main>
                 <iframe title="Report Section"  
                 src="https://app.powerbi.com/view?r=eyJrIjoiZTk1NmM2MTAtMDc5NS00MzM3LThmM2UtNjhmMzc4ZDAzYmI3IiwidCI6ImEyNTg2YjliLWY4NjctNGIzYy05MzYzLTViNDM1YzVkYmM0NSIsImMiOjh9&pageName=ReportSectiona15dd370f06576987a76" 
-                frameBorder="0" allowFullScreen={true}>
+                 allowFullScreen={true} >
                 </iframe>
             </main>
 }
